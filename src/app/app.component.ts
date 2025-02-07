9d8fcb5de3f1b9d70a65ee4cd654a58fad7f8c1c
Aurélien Gallea <a.gallea@transcausse.com>
2025-02-07 16:15:52 +0100

    chore(app): add firebase staging configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  readonly isStagging = !(environment as any).production
 }
