3c042f6097daf19eb396058ccfccdf7433f29f25
aurelien-gallea <121095266+aurelien-gallea@users.noreply.github.com>
2025-02-10 10:24:27 +0100

    Multiple environment (#3)
    Co-authored-by: Aurélien Gallea <a.gallea@transcausse.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,5 @@ import { environment } from '../environments/environment';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly isStagging = !(environment as any).production
+
 }
