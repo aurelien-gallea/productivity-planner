ee057eb133549098dd34084970d34a448779f9fc
Aurélien Gallea <a.gallea@transcausse.com>
2025-02-11 08:57:53 +0100

    feat(auth): add save method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthenticationService } from './core/authentication.service';
+import { switchMap } from 'rxjs';
 
 
 @Component({
@@ -11,5 +13,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
 
+readonly #authService = inject(AuthenticationService);
+
+  onClickConnect() {
+    this.#authService.login('jojo.lasticot@gmail.com', 'azerty').pipe(
+      switchMap((r) => {
+        console.log(r);
+        
+        const { email, localId, idToken } = r;
+       return this.#authService.save(email, localId, idToken)    
+      })
+    ).subscribe(
+      (r) => console.log(r)
+      
+    )
+}
+
+  
 
 }
