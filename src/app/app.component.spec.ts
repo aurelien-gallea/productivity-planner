93c6369665f78b1b055d75845437f4b5ca1be86e
aurelien-gallea <121095266+aurelien-gallea@users.noreply.github.com>
2025-02-11 09:38:50 +0100

    Auth firebase2 (#6)
    Co-authored-by: Aurélien Gallea <a.gallea@transcausse.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { AuthenticationService } from './core/authentication.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent,],
+      providers: [provideHttpClient(), provideHttpClientTesting(), AuthenticationService]
     }).compileComponents();
   });
 
