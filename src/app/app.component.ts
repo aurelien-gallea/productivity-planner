3a0391da91daeaa14c224fccf66a2af15d7745fc
Aurélien Gallea <a.gallea@transcausse.com>
2025-02-24 17:54:14 +0100

    wip(home-features): setup input property and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AuthenticationService } from './core/authentication.service';
-import { switchMap } from 'rxjs';
+
 
 
 @Component({
@@ -13,21 +12,9 @@ import { switchMap } from 'rxjs';
 })
 export class AppComponent {
 
-readonly #authService = inject(AuthenticationService);
 
-  onClickConnect() {
-    this.#authService.login('jojo.lasticot@gmail.com', 'azerty').pipe(
-      switchMap((r) => {
-        console.log(r);
-        
-        const { email, localId, idToken } = r;
-       return this.#authService.save(email, localId, idToken)    
-      })
-    ).subscribe(
-      (r) => console.log(r)
-      
-    )
-}
+
+  
 
   
 
--- src/app/visitor/home/homepage-feature-card-list/homepage-feature-card-list.dumb.component.ts
+++ src/app/visitor/home/homepage-feature-card-list/homepage-feature-card-list.dumb.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-homepage-feature-card-list',
@@ -8,5 +8,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomepageFeatureCardListDumbComponent {
-
+  readonly featureCardList = input.required();
 }
