0ec07a2a96d14d316f0ac79e5237e644561e7fe4
Aurélien Gallea <a.gallea@transcausse.com>
2025-02-24 17:43:15 +0100

    wip(home-banner): add unit test

--- src/app/visitor/home/home.page.component.ts
+++ src/app/visitor/home/home.page.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { HomeBannerDumbComponent } from "./home-banner/home-banner.dumb.component";
 
 @Component({
-  imports: [],
+  imports: [HomeBannerDumbComponent],
   templateUrl: './home.page.component.html',
   styleUrl: './home.page.component.scss'
 })
 export class HomePageComponent {
+onBannerCLicked() {
+console.log('bannerCLicked');
+
+}
+
 
 }
