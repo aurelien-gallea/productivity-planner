401134aac2849e7659ada1644f2bea4c9ed32ea9
Aurélien Gallea <a.gallea@transcausse.com>
2025-04-14 17:22:59 +0200

    feat(auth): implement DIP on auth service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { AuthenticationService } from './core/authentication.service';
+import { AuthenticationFirebaseService } from './core/authentication-firebase.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(), 
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    {
+      provide: AuthenticationService,
+      useClass: AuthenticationFirebaseService
+    }
+  ]
 };
--- src/app/core/authentication.service.ts
+++ src/app/core/authentication.service.ts
@@ -1,65 +1,25 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-/**
- * @see https://firebase.google.com/docs/reference/rest/auth?hl=fr#section-create-email-password
- */
-interface FirebaseResponseSignUp {
-  idToken : string;
-  email: string;
-  refreshToken : string;
-  expiresIn: string,
-  localId: string;
-}
-
-interface FirebaseResponseSignIn {
-  displayName: string;
-  email: string;
+export interface RegisterResponse {
+  jwtToken: string;
+  jwtRefreshToken: string;
   expiresIn: string;
-  idToken : string;
-  kind : string;
-  localId: string;
-  refreshToken : string;
-  registered: boolean;
+  userId: string;
 }
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthenticationService {
-
-  
-  readonly #http = inject(HttpClient);
-  
-  register(email: string, password : string): Observable<FirebaseResponseSignUp> {
-    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseConfig.apiKey}`;
-    const body = {email, password, returnSecureToken:true};
-    return this.#http.post<FirebaseResponseSignUp>(url, body);
-  }
-
-  login(email: string, password : string) : Observable<FirebaseResponseSignIn> {
-    const url =`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseConfig.apiKey}`;
-    const body = {email, password, returnSecureToken:true};
-    return this.#http.post<FirebaseResponseSignIn>(url, body);
-  }
-
-  save(email : string, userId: string, bearerToken: string) {
-    const baseUrl = `https://firestore.googleapis.com/v1/projects/${environment.firebaseConfig.projectId}/databases/(default)/documents`
-    const userFirestoreCollectionId = 'users';
-    const url = `${baseUrl}/${userFirestoreCollectionId}?key=${environment.firebaseConfig.apiKey}&documentId=${userId}`
-    const body = {
-      fields : {
-        email: { stringValue: email }
-      }
-    }
-
-    const headers = new HttpHeaders({
-      'Content-type' : 'application/json',
-      'Authorization': `Bearer ${bearerToken}`
-    })
-    
-    return this.#http.post<unknown>(url, body, {headers})
+export interface LoginResponse {
+  jwtToken: string;
+  jwtRefreshToken: string;
+  expiresIn: string;
+  userId: string;
+  isRegistered: boolean;
 }
+
+@Injectable()
+export abstract class AuthenticationService {
+  abstract register(
+    email: string,
+    password: string
+  ): Observable<RegisterResponse>;
 }
