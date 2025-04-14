32d5b8a5a66a9fa7043821452fb26aa909c52337
Aurélien Gallea <a.gallea@transcausse.com>
2025-04-14 10:46:15 +0200

    feat(navbar): generate svg component for webapp logo

---
+++ src/app/core/navbar/navbar-logo/navbar-logo.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-logo',
+  imports: [],
+  templateUrl: './navbar-logo.component.svg',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarLogoComponent { }
